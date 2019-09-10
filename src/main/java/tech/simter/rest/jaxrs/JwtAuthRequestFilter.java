f71b642c31e042aba68586041b658d6bde41b63f
RJ <rongjihuang@gmail.com>
2019-09-10 18:12:38 +0800

    Add `simter.jwt.exclude-paths` prop config to JwtAuthRequestFilter

--- src/main/java/tech/simter/rest/jaxrs/JwtAuthRequestFilter.java
+++ src/main/java/tech/simter/rest/jaxrs/JwtAuthRequestFilter.java
@@ -10,19 +10,23 @@ import tech.simter.jwt.JWT;
 import javax.annotation.Priority;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
+import java.util.List;
 
 /**
  * Verify the JWT Authorization info, then set all payload data into {@link ThreadLocal} variable.
  * <p>
  * App can get the variable value through {@link Context#get(String)} method.
- * Throws {@link DecodeException} If JWT verify failed.
+ * <p>
+ * Abort with {@link Status#UNAUTHORIZED} if without a jwt type `Authorization` header.
+ * Throws {@link DecodeException} If JWT header verified failed.
  *
  * @author RJ
  */
@@ -32,51 +36,76 @@ import java.io.IOException;
 @Priority(Priorities.AUTHENTICATION)
 public class JwtAuthRequestFilter implements ContainerRequestFilter {
   private static Logger logger = LoggerFactory.getLogger(JwtAuthRequestFilter.class);
-  /** The header name to hold JWT token */
+  /**
+   * The header name to hold JWT token
+   */
   public static final String JWT_HEADER_NAME = "Authorization";
-  /** The prefix of the jwt header value */
-  public static final String JWT_VALUE_PREFIX = "Bearer "
-  private final String secretKey;
-  private final boolean requireAuthorized;
+  /**
+   * The prefix of the jwt header value
+   */
+  public static final String JWT_VALUE_PREFIX = "Bearer ";
+  private String secretKey;
+  private boolean requireAuthorized;
+  private List<String> excludePaths;
+  private boolean hasExcludePaths;
 
   /**
-   * @param secretKey           the secret-key
+   * @param secretKey         the secret-key
    * @param requireAuthorized true to abort request if the JWT verified failed
    */
   public JwtAuthRequestFilter(
     @Value("${simter.jwt.secret-key:test}") String secretKey,
-    @Value("${simter.jwt.require-authorized:false}") boolean requireAuthorized
-    ) {
+    @Value("${simter.jwt.require-authorized:false}") boolean requireAuthorized,
+    @Value("${simter.jwt.exclude-paths:#{null}}") List<String> excludePaths
+  ) {
     this.secretKey = secretKey;
     this.requireAuthorized = requireAuthorized;
+    this.excludePaths = excludePaths;
+    this.hasExcludePaths = excludePaths != null && !excludePaths.isEmpty();
   }
 
   @Override
-  public void filter(ContainerRequestContext c) throws IOException {
+  public void filter(ContainerRequestContext c) {
+    if (!requireAuthorized
+      || HttpMethod.OPTIONS.equals(c.getMethod())
+      || isExcludePath(c.getUriInfo().getPath())) {
+      return;
+    }
+
+    // need authorized
     String authorization = c.getHeaderString(JWT_HEADER_NAME);
-    if (authorization == null || authorization.isEmpty() || !authorization.startsWith(JWT_VALUE_PREFIX)) {
-      if (requireAuthorized) abortWithForbidden(c, "No valid jwt 'Authorization' header");
+    if (authorization == null || !authorization.startsWith(JWT_VALUE_PREFIX)) {
+      abortRequest(c, Status.UNAUTHORIZED, "No valid jwt 'Authorization' header");
     } else {
       try {
         // verify and decode to a JWT instance
-        authorization = authorization.substring(7);
+        authorization = authorization.substring(7); // "Bearer ".length() == 7
         logger.debug("jwt={}", authorization);
-        JWT jwt = JWT.verify(authorization, secretKey);  // "Bearer ".length() == 7;
+        JWT jwt = JWT.verify(authorization, secretKey);
 
         // set all payload data to context
         jwt.payload.getData().forEach(Context::set);
       } catch (DecodeException e) {
         if (logger.isDebugEnabled()) logger.debug(e.getMessage(), e);
         else logger.warn(e.getMessage());
-        if (requireAuthorized) abortWithForbidden(c, "Invalid JWT");
+        abortRequest(c, Status.UNAUTHORIZED, "Invalid JWT");
       }
     }
   }
 
-  private void abortWithForbidden(ContainerRequestContext c, String msg) {
-    c.abortWith(Response.status(Response.Status.FORBIDDEN)
+  private boolean isExcludePath(String path) {
+    if (isRootPath(path)) return true;
+    else return hasExcludePaths && excludePaths.stream().anyMatch(path::startsWith);
+  }
+
+  private boolean isRootPath(String path) {
+    return path.isEmpty() || path.equals("/") || path.equals("/index.html") || path.equals("/index.htm");
+  }
+
+  private void abortRequest(ContainerRequestContext c, Status status, String body) {
+    c.abortWith(Response.status(status)
       .type(MediaType.TEXT_PLAIN)
-      .entity(msg)
+      .entity(body)
       .build()
     );
   }
